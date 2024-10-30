60cd9eca0f8795ebdd8a2687661ba2cfd264a8db
polkadavid <polkadavid@gmail.com>
2024-10-30 11:23:07 +0000

    working on landing

--- app/pages/repertori/index.vue
+++ app/pages/repertori/index.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col justify-center text-center">
     <h1 class="text-3xl font-bold my-8 underline">Repertori</h1>
     <ul>
-      <li v-for="song in repertoriStore.songs" :key="song.id" class="mb-2">
+      <li v-for="song in repertoriStore.songs" :key="song.id" class="mb-4">
         <NuxtLink 
           :to="{ name: 'repertori-id', params: { id: song.id } }"
           class="hover:underline"
